5a687bf04d7b1449545b58618697c9d471fcd98f
Bernie Truong <tqtolympia@gmail.com>
2025-01-01 16:29:53 +0700

    [1422] [Time Beats: 97.36%] [Memory Beats: 36.08%] - LeetPush

---
+++ 1422-Maximum-Score-After-Splitting-a-String.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxScore(String s) {
+        int zeros = 0, ones = 0, best = Integer.MIN_VALUE;
+        for(int i = 0; i < s.length() - 1; i++){
+            if(s.charAt(i) == '1') ones++;
+            else zeros++;
+            best = Math.max(best, zeros - ones);
+        }
+        if(s.charAt(s.length() - 1) == '1') ones++;
+        return best + ones;
+    }
+}
